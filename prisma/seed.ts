18ae260a449c735cb7f1d4b63cf8da808ffc1248
Habasch Parwar, GHR-POD-NEX-18 <parwar.habasch@gmx.de>
2023-06-16 14:29:33 +0200

    feat: added a new card to db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 async function main() {
-  const cardOne = await prisma.yuGiOhCard.create({
+  const DarkMagician = await prisma.yuGiOhCard.create({
     data: {
       name: 'Dark Magician',
       level: 7,
@@ -22,9 +22,8 @@ async function main() {
       },
     },
   });
-  console.log(cardOne);
 
-  const cardTwo = await prisma.yuGiOhCard.create({
+  const BlueEyesWhiteDragon = await prisma.yuGiOhCard.create({
     data: {
       name: 'Blue-Eyes White Dragon',
       level: 8,
@@ -45,9 +44,7 @@ async function main() {
     },
   });
 
-  console.log(cardTwo);
-
-  const cardThree = await prisma.yuGiOhCard.create({
+  const RedEyesBlackDragon = await prisma.yuGiOhCard.create({
     data: {
       name: 'Red-Eyes Black Dragon',
       level: 7,
@@ -63,6 +60,27 @@ async function main() {
         create: {
           imageUrl:
             '/Users/taapaha6/Documents/dev/Yu-Gi-Oh-searchEngine/search-engine/public/red_eyes_black_dragon.jpeg',
+        },
+      },
+    },
+  });
+
+  const OjamaYellow = await prisma.yuGiOhCard.create({
+    data: {
+      name: 'Ojama Yellow',
+      level: 2,
+      desc: 'A yellow Ojama, with a carefree personality. His shameless smile is his biggest weapon.',
+      atk: 0,
+      def: 1000,
+      extraDeck: false,
+      cardType: 'MONSTER',
+      monsterType: 'Beast',
+      monsterSubType: 'Normal',
+      monsterAttribute: 'Light',
+      YuGiOhCardImage: {
+        create: {
+          imageUrl:
+            '/Users/taapaha6/Documents/dev/Yu-Gi-Oh-searchEngine/search-engine/public/ojama_yellow.webp',
         },
       },
     },
